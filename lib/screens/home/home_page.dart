0f49ff2d0915747e850523917ce37ccb5da9675a
Christian Benseler <cbenseler@gmail.com>
2019-11-26 18:43:24 -0300

    feat: better layout home

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -3,43 +3,42 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
 
-  Container _menuItem(String label, String url, Icon icon, context) {
-    return new Container(
-        padding: EdgeInsets.all(20.0),
-        child: new RaisedButton(
-          color: Colors.white70,
-          shape: RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(200.0),
-            side: BorderSide(color: Colors.yellow)
-          ),
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              icon,
-              new Text(label, style: TextStyle(fontSize: 24.0))
-            ],
-          ),
-          onPressed: () => Navigator.of(context).pushNamed(url),
-      )
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    List<dynamic> items  = [
+      ['Planets', '/planets', Icons.location_on],
+      ['Movies', '/movies', Icons.movie]
+    ];
     return Container(
       child: new Scaffold(
         appBar: new AppBar(
           title: new Text('Star Wars API'),
         ),
-        body: GridView.count(
-          padding: EdgeInsets.only(top: 32.0),
-          crossAxisCount: 2,
-          children: <Widget>[
-            _menuItem('Planets', '/planets', new Icon(Icons.location_on, size: 48.0,), context),
-            _menuItem('Movies', '/movies', new Icon(Icons.movie, size: 48.0,), context),
-          ],
-        )
+        backgroundColor: Colors.black45,
+        
+        body: ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: items.length,
+          itemBuilder: (BuildContext ctx, int index) {
+
+            return Card(
+              elevation: 8.0,
+              margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.grey),
+                child: new ListTile(
+                  title: new Text(items[index][0], style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  leading: new Icon(items[index][2], color: Colors.white),
+                  onTap: () {
+                    Navigator.of(context).pushNamed(items[index][1]);
+                  },
+                  trailing: Icon(Icons.keyboard_arrow_right, color: Colors.white,),
+                  
+                ),
+              ),
+            );
+          },
+        ),
       )
     );
   }
